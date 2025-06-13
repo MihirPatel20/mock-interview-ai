40ba19bdda0d9360bd0c3b44a7dd3bb16b493728
mihir patel <mihirpatel1150@gmail.com>
2025-06-13 22:10:57 +0530

    implement feedback generation and display it after taking an interviews

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -4,9 +4,19 @@ import { Button } from "./ui/button";
 import Link from "next/link";
 import { getRandomInterviewCover } from "@/lib/utils";
 import DisplayTechIcons from "./DisplayTechIcons";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
 
-const InterviewCard = ({ id, role, type, techstack, createdAt }: InterviewCardProps) => {
-  const feedback = null as Feedback | null;
+const InterviewCard = async ({
+  id,
+  userId,
+  role,
+  type,
+  techstack,
+  createdAt,
+}: InterviewCardProps) => {
+  const feedback =
+    userId && id ? await getFeedbackByInterviewId({ interviewId: id, userId }) : null;
+
   const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
   const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format("MMM D, YYYY");
 
@@ -51,7 +61,7 @@ const InterviewCard = ({ id, role, type, techstack, createdAt }: InterviewCardPr
 
           <Button className="btn-primary">
             <Link href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}>
-              {feedback ? "Check Feedback" : "View Interview"}
+              {feedback ? "Check Feedback" : "Take an Interview"}
             </Link>
           </Button>
         </div>
